0d932385a7ebbe280c3bf04078e28e7b451dde58
demonarch <88692264+demonarch@users.noreply.github.com>
2024-03-04 14:43:04 +0100

    Added demo trading (Not tested)

--- bingX/spot/Spot.py
+++ bingX/spot/Spot.py
@@ -8,11 +8,19 @@ class Spot(API):
     def __init__(self,
         api_key:    str,
         api_secret: str,
+        mode:       str = 'demo'  # Default mode is 'demo'
     ) -> object:
+        if mode == "real":
+            url = "https://open-api.bingx.com"
+        elif mode == "demo":
+            url = "https://open-api-vst.bingx.com"
+        else:
+            raise ValueError("Invalid mode. Mode must be 'real' or 'demo'.")
+        
         super().__init__(
             api_key    = api_key,
             api_secret = api_secret,
-            base_url   = 'https://open-api.bingx.com',
+            base_url   = url,
         )
 
     # ========== TRADE INTERFACE ==========
